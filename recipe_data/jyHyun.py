6ba5e4657b7a509650f2b10deec54e47c72a0ec9
jin-ee <jinyi479@gmail.com>
2022-06-27 19:08:41 +0900

    jy selenium setting

--- recipe_data/jyHyun.py
+++ recipe_data/jyHyun.py
@@ -14,7 +14,13 @@ for category in categories:
     menu_ul.extend(soup.select('ul[class="tag_cont"] > li > a'))
     for menu in menu_ul:
         menu_list.append(menu.text)
-    
-print(menu_list)
-        
-      
+# print(menu_list)
+
+# 정확순 정렬 메뉴 리스트 > 1번째 객체
+for menu in menu_list:
+    target_url = f'https://www.10000recipe.com/recipe/list.html?q={menu}&query=&cat1=&cat2=&cat3=&cat4=&fct=&order=accuracy&lastcate=order&dsearch=&copyshot=&scrap=&degree=&portion=&time=&niresource='
+    resp = requests.get(target_url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+    top_menu = soup.select('ul[class="common_sp_list_ul ea4"] > li')[0]
+    print(len(top_menu))
