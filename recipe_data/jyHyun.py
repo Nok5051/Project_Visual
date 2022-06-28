d5712b77ac172a05adcd395dac59fc38537f9ac5
jin-ee <jinyi479@gmail.com>
2022-06-28 09:39:34 +0900

    Null exception

--- recipe_data/jyHyun.py
+++ recipe_data/jyHyun.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from time import sleep
+import re
+from pandas import DataFrame
+
 import json
 
 service = Service('../drivers/chromedriver.exe')
@@ -12,6 +15,7 @@ driver = webdriver.Chrome(service=service)
 categories = [53, 54, 55, 56, 61, 65]
 menu_ul = list()
 menu_list = list()
+servings = list()
 ingredients = list()
 units = list()
 recipes = list()
@@ -27,11 +31,11 @@ for category in categories:
 
 # print(menu_list) - 파스타, 라면 , ... 총 622개
 
-# test_menu = menu_list[0:2]
-# for menu in test_menu:
+test_menu = menu_list[0:2]
+for menu in test_menu:
 
 # 메뉴 정확순 정렬
-for menu in menu_list:
+# for menu in menu_list:
     url = f'https://www.10000recipe.com/recipe/list.html?q={menu}&query=&cat1=&cat2=&cat3=&cat4=&fct=&order=accuracy&lastcate=order&dsearch=&copyshot=&scrap=&degree=&portion=&time=&niresource='
     driver.get(url)
     sleep(3)
@@ -44,10 +48,45 @@ for menu in menu_list:
     resp = requests.get(recipe_url)
     soup = BeautifulSoup(resp.text, 'html.parser')
 
-    ingredients.append(soup.select('#divConfirmedMaterialArea > ul > a > li'))
+    servings.append(soup.find('span', class_='view2_summary_info1').text)
+
+    recipe = soup.select('div[class="media-body"]')
+    recipe_n = list()
+    # 전처리
+    for i in recipe:
+        if i.has_attr('id'):
+            recipe_n.extend(i)
+    recipes.append(recipe_n)
+
+    ingredient = soup.select('#divConfirmedMaterialArea > ul > a > li')
+    # 전처리
+    for i in ingredient:
+        i.find("img").decompose()
+        i.find("a").decompose()
+        i.find("span").decompose()
+    ingredients.append(ingredient)
+    
     units.append(soup.select('span[class="ingre_unit"]'))
-    recipes.append(soup.select('div[class="media-body"]'))
+
+
+
+# print(test_menu)
+# print(servings)
+# print(recipes)
 
 # print(ingredients)
 # print(units)
-# print(recipes)
+
+
+
+# 레시피 데이터프레임 - 메뉴명, 인분, 레시피
+# data = {'RECIPE_NM': test_menu, 'QNT': servings, 'RECIPE': recipes}
+# df = DataFrame(data)
+# with open('./recipe_table.json', 'w', encoding='utf-8') as file:
+#     df.to_json(file, force_ascii=False)
+
+# 재료 데이터프레임 - 메뉴명, 재료, 용량
+# data = {'RECIPE_NM': test_menu, 'INGREDIENTS': ingredients, 'UNITS': units}
+# df = DataFrame(data)
+# with open('./recipe_ingredient_table.json', 'w', encoding='utf-8') as file:
+#     df.to_json(file, force_ascii=False)
