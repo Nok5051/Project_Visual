a1b51adb92770cfb99d8f89c066f18eb12f68dc6
YuhaShin <newcrystal1994@gmail.com>
2022-06-30 16:44:22 +0900

    add data_insert2.py

---
+++ recipe_data/data_insert2.py
@@ -0,0 +1,31 @@
+import csv
+import pymysql
+
+db = pymysql.connect(host='localhost', port=3306, user='mealproject', password='meal1234', db='MEAL', charset='utf8')
+cursor = db.cursor(pymysql.cursors.DictCursor)
+
+# 테이블 생성
+create_sql = '''
+CREATE TABLE ingredient_price(
+    ID INT(200) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    INGREDIENT_NM VARCHAR(20) NOT NULL,
+    UNIT VARCHAR(10) NOT NULL,
+    PRICE VARCHAR(20) NOT NULL
+)
+'''
+cursor.execute(create_sql)
+
+# 데이터 삽입
+insert_sql = '''
+INSERT INTO ingredient_price(INGREDIENT_NM, UNIT, PRICE) values (%s, %s, %s)
+'''
+
+f = open('standard_price.csv', 'r', encoding='utf-8-sig')
+rd = csv.reader(f)
+
+for line in rd:
+    cursor.execute(insert_sql, (line[0], line[1], line[2]))
+
+db.commit()
+db.close()
+f.close()
