ce4ed3b17a2ef02d25dd85f48a436a512c698bde
jin-ee <jinyi479@gmail.com>
2022-07-01 10:16:01 +0900

    import mysql

--- mealproject/mealproject/views.py
+++ mealproject/mealproject/views.py
@@ -3,15 +3,7 @@ import csv
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 # views
-from .models import RecipeTable
+# from .models import RecipeTable
 
 def index(request):
-<<<<<<< HEAD
-    return render(request, 'index.html')
-
-
-def csvLoader(request):
-    return HttpResponse('create model <br> upload complete')
-=======
     return render(request, 'mealproject/index.html')
->>>>>>> da601e10bb8ae4a873ee02e3d95c613bc64bf635
