570b01b2f0b85b1d1c581e402b113aa800a428d5
jin-ee <jinyi479@gmail.com>
2022-07-01 09:37:19 +0900

    models.py test

--- mealproject/recipe/models.py
+++ mealproject/recipe/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    RECIPE_NM = models.CharField(max_length=50)
+    def __str__(self):
+        return str({'RECIPE_NM': self.RECIPE_NM})
