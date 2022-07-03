f406f19771b390bea88e109a9e359ea1a8f1c913
jin-ee <jinyi479@gmail.com>
2022-07-03 15:28:49 +0900

    unit-price complete - add serializer

--- mealproject/recipe/serializers.py
+++ mealproject/recipe/serializers.py
@@ -3,11 +3,6 @@ from .models import *
 
 # serializer : 직렬화 - 받아온 데이터를 django에서 처리할 수 있는 데이터형으로 변환
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ('category')
-
 class CategorySerializer(serializers.ModelSerializer):
     category = models.CharField(db_column='CATEGORY', max_length=10)
     class Meta:
