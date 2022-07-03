3b2424f1cc9ae2a8bf537ff206fc7f00f6091ded
YuhaShin <newcrystal1994@gmail.com>
2022-07-03 11:16:08 +0900

    changed recipe index

--- mealproject/recipe/serializers.py
+++ mealproject/recipe/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
 
+# serializer : 직렬화 - 받아온 데이터를 django에서 처리할 수 있는 데이터형으로 변환
+
 # class CategorySerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Category
