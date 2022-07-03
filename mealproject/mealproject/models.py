b448e2c2570b167aa8d3f65bfd34403317818aa4
jin-ee <jinyi479@gmail.com>
2022-07-03 14:16:10 +0900

    wordcloud added

--- mealproject/mealproject/models.py
+++ mealproject/mealproject/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 
-# class IngredientPrice(models.Model):
-#     ingredient_name = models.CharField(max_length=200)
-#     unit = models.CharField(max_length=100)
-#     unitprice = models.IntegerField()
-#     def __str__(self):
-#         return str({'ingredient_name': self.ingredient_name,'unit': self.unit,'unitprice': self.unitprice})
-
-
-# class RecipeTable(models.Model):
-#     RECIPE_NM = models.CharField(max_length=20)
-#     QNT = models.CharField(max_length=10)
-#     RECIPE = models.CharField(max_length=2000)
-#     def __str__(self):
-#         return str({'RECIPE_NM': self.RECIPE_NM,'QNT': self.QNT,'RECIPE': self.RECIPE})
-
-
-
