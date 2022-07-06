d2acfa50ff2cda7925b0f462388179d2f087750e
jin-ee <jinyi479@gmail.com>
2022-07-06 11:35:50 +0900

    data_insert fix

--- recipe_data/data_insert.py
+++ recipe_data/data_insert.py
@@ -5,7 +5,7 @@ import csv
 conn = pymysql.connect(host='50.18.18.112', user='mealproject', password='meal1234', db = 'meal', charset='utf8') 
 curs = conn.cursor(pymysql.cursors.DictCursor) 
 
-# 테이블 생성
+# 레시피 테이블 생성
 create_sql = '''
 CREATE TABLE Recipe(
     ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -19,9 +19,9 @@ CREATE TABLE Recipe(
 );
 
 '''
-curs.execute(create_sql) 
+# curs.execute(create_sql) 
 
-# 데이터 삽입
+# 레시피 데이터 삽입
 insert_sql = '''
 INSERT INTO Recipe(RECIPE_NM, CATEGORY, QNT, RECIPE, INGREDIENTS, UNITS, TOTAL_PRICE) VALUES (%s,%s,%s,%s,%s,%s,%s);
 
@@ -30,28 +30,24 @@ INSERT INTO Recipe(RECIPE_NM, CATEGORY, QNT, RECIPE, INGREDIENTS, UNITS, TOTAL_P
 f = open('./recipe_data/df_rc.csv', 'r', encoding='utf-8-sig')
 rd = csv.reader(f)
 
-for line in rd:
-    curs.execute(insert_sql, (line[1], line[2], line[3], line[4], line[5], line[6], line[7]))
+# for line in rd:
+#     curs.execute(insert_sql, (line[1], line[2], line[3], line[4], line[5], line[6], line[7]))
 
 
-# 카테고리 테이블
+# 카테고리 테이블 생성
 sql = '''
 
-CREATE TABLE Category(
+CREATE TABLE Category_test(
     ID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     CATEGORY VARCHAR(10) NOT NULL
 );
-
 '''
-
-category_intertsql = '''
-insert into Category(CATEGORY)
-select distinct CATEGORY from Recipe
-where CATEGORY not in (select CATEGORY from Category);
-);
-'''
-
 # curs.execute(sql)
+
+insert_sql = '''
+insert into Category_test(CATEGORY) select distinct CATEGORY from Recipe where "CATEGORY" not in (select CATEGORY from Category_test);
+'''
+# curs.execute(insert_sql)
 
 conn.commit()
 conn.close()
